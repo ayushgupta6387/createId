be1ba1f31bac6abf6b864d79bd4544b9b0bb99b7
AyushGupta <ayush.gupta1cs18@gla.ac.in>
2020-02-27 23:20:45 -0800

    create method for generate random pasword

--- src/main/Solution.java
+++ src/main/Solution.java
@@ -1,5 +1,7 @@
 package main;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Solution {
     private String userName;
     private int userID;
@@ -25,6 +27,11 @@ public class Solution {
 
     private String createId() {
         return String.format("%s_%d@%s", getUserName(), getUserID(), getCompanyName());
+    }
+
+    private int generatePassword() {
+        int number = ThreadLocalRandom.current().nextInt(1000, 9999);
+        return number;
     }
 
 
