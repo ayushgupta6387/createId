85659762ade82ad702bd34c9d9c625bcbb8efef1
AyushGupta <ayush.gupta1cs18@gla.ac.in>
2020-02-27 23:33:35 -0800

    take input for the employees to create Id's according to them

--- src/main/UserID.java
+++ src/main/UserID.java
@@ -5,6 +5,9 @@ import java.util.Scanner;
 public class UserID {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number of employees");
+        int employee = scanner.nextInt();
+        scanner.nextLine();
 
     }
 }
