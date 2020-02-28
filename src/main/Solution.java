6693b37f88b8d752f4c5c2a4f98dc4efd0abe165
AyushGupta <ayush.gupta1cs18@gla.ac.in>
2020-02-27 22:45:20 -0800

    create parametrized constructor

--- src/main/Solution.java
+++ src/main/Solution.java
@@ -5,4 +5,9 @@ public class Solution {
     private int userID;
     private String companyName;
 
+    public Solution(String userName, int userID, String companyName) {
+        this.userName = userName;
+        this.userID = userID;
+        this.companyName = companyName;
+    }
 }
